5c45cd5490a5c0f69542c03617c269056c0a35e7
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-04-30 22:45:05 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove()
+
+
+//function newHeader(){
+const newHeader = document.createElement('h1');
+
+newHeader.id='victory'
+
+
+newHeader.textContent =' Bernice is the champion'
+
+//let h1= newHeader.nodeName
